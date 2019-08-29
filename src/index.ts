8c778b2c3ca923c58c0b0fbbcddc126bdb130bb5
Boris Polak <boris.polak@icloud.com>
2019-08-29 20:09:12 +0200

    Fix typeDefs path

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { GraphQLServer } from 'graphql-yoga'
 import { prisma } from './generated/prisma-client'
 import { authMiddleware } from './middlewares'
 import resolvers from './resolvers'
+import 'graphql-import-node'
+import * as typeDefs from './schema.graphql'
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs,
   resolvers,
   middlewares: [authMiddleware],
   context: request => ({
@@ -13,4 +15,4 @@ const server = new GraphQLServer({
   }),
 })
 
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start({ endpoint: '/graphql' }, () => console.log(`Server is running on http://localhost:4000`))
